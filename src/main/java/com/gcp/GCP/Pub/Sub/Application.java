e8c3a5edb81cd07a175bbb1208ee90a54fe6e2fb
Himanshu <38362189+himanshu1755@users.noreply.github.com>
2023-05-22 12:06:19 +0530

    Project initialization

---
+++ src/main/java/com/gcp/GCP/Pub/Sub/Application.java
@@ -0,0 +1,13 @@
+package com.gcp.GCP.Pub.Sub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
